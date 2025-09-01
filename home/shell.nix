358f475af49e9433a2256a2596de88c06a4db5fe
Vasilii Nechaev <vn.glider@gmail.com>
2025-09-01 12:20:14 -0700

    enable direnv

--- home/shell.nix
+++ home/shell.nix
@@ -1,36 +1,42 @@
 { ... }:
 {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    initContent = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-    '';
-  };
-
 #  home.shellAliases = {
 #    k = "kubectl";
-
-#    urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-#    urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
 #  };
 
-  programs.starship = {
-    enable = true;
+  programs = {
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
 
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    enableNushellIntegration = true;
+    starship = {
+      enable = true;
 
-    settings = {
-      character = {
-        success_symbol = "[›](bold green)";
-        error_symbol = "[›](bold red)";
-      };
-      aws = {
-        symbol = "🅰 ";
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableNushellIntegration = true;
+
+      settings = {
+        character = {
+          success_symbol = "[›](bold green)";
+          error_symbol = "[›](bold red)";
+        };
+        aws = {
+          symbol = "🅰 ";
+        };
       };
     };
+
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      initContent = ''
+        export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+      '';
+    };
+
   };
 
 }
